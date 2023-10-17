2335e8ff73a69e31fda3da75d092f88ec5cf5169
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2023-10-17 14:12:51 -0400

    upd folder

--- miniCourseHandsMoves/app.py
+++ miniCourseHandsMoves/app.py
@@ -1,25 +1,26 @@
-import cv2 
 from cvzone.HandTrackingModule import HandDetector
+import cv2
 
 camera = cv2.VideoCapture(0)
-#camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1280)
-#camera.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
 detector = HandDetector(maxHands=2)
 
 while(True):
-    
+
     _,imagem_de_entrada = camera.read()
     lista_maos = detector.findHands(imagem_de_entrada, draw= True)
 
     cv2.imwrite('saida.jpg', imagem_de_entrada)
-    # if lista_maos:
-    #     primeira_mao = lista_maos[0]
-    #     dedinhos = detector.fingersUp(primeira_mao[0])    
+    if lista_maos:
+         primeira_mao = lista_maos[0]
+
+         if(primeira_mao):
+            center1 = primeira_mao[0]['center']
+            print(center1)
+            fingers = detector.fingersUp(primeira_mao[0])
+
+
     cv2.imshow('imagem', imagem_de_entrada)
     if(cv2.waitKey(1) & 0xFF == ord("q")):
         break
-#imagem_de_saida  = cv2.imread('saidajpg')
-#imagem_de_saida = imagem_de_saida [0,:,:] + 70
-#cv2.imwrite('saida.jpg', imagem_de_saida)
-    
+
 camera.release()
